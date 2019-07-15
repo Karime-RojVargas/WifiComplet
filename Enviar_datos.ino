ed48a11e343fe35436794dfbfe69909f743f715b
Karime-RojVargas <52900618+Karime-RojVargas@users.noreply.github.com>
2019-07-15 14:01:54 -0500

    Add files via upload

---
+++ Enviar_datos.ino
@@ -0,0 +1,58 @@
+#include <SoftwareSerial.h>
+SoftwareSerial ESP(2, 3); // Pin 2 (RX) y Pin 3 (TX)
+String W = " ";
+char w = ' ';
+int boton=5;
+void setup()
+{
+  pinMode(boton,INPUT);
+Serial.begin(9600);
+ESP.begin(9600);
+}
+void loop()
+{
+if (ESP.available()) // Lo que entra por WIFI a Serial
+{
+w = ESP.read() ;
+Serial.print(w);
+W = W + w; // Se va acumulando un String con lo que entra
+}
+
+int estado = digitalRead(boton);
+// Condicional para saber estado del pulsador
+
+
+
+
+if (Serial.available()) // Lo que entra por Serial a WIFI
+{
+char s = Serial.read();
+ESP.print(s);
+}
+if ( estado == 0 ) // Comprobar si la cadena "enter" está incluida en el String
+{
+ESP.println("AT+CIPSEND=0,21");
+
+if (ESP.find(">")) // Si se recibe la petición del mensaje (“>”)
+{
+Serial.println("Enviando al Navegador...");
+ESP.println("Boton no presionado\n"); // String a enviar al navegador
+
+
+} //del if
+}
+else
+{
+  if ( estado == 1 ) // Comprobar si la cadena "enter" está incluida en el String
+{
+ESP.println("AT+CIPSEND=0,18");
+
+if (ESP.find(">")) // Si se recibe la petición del mensaje (“>”)
+{
+Serial.println("Enviando al Navegador...");
+ESP.println("Boton presionado\n"); // String a enviar al navegador
+
+}
+}
+}
+}//de loop
---
+++ configwifi.ino
@@ -0,0 +1,33 @@
+#include "SoftwareSerial.h"
+
+SoftwareSerial wifi (2, 3); // RX | TX //se nombra el módulo ESP8266 como “wifi”
+
+char txt; // Se crea una variable tipo char donde se almacene el texto que se leerá
+
+void setup()
+
+{ Serial.begin(9600); //Inicia el monitor serial a 9600 baudios
+
+wifi.begin(9600); // Se inicia el módulo wifi a una velocidad de 115200 baudios que es la lectura que soporta y está configurado el ESP8266
+
+}
+
+void loop()
+
+{if (Serial.available()) //Si hay datos, envialos cuando los reciba
+
+{txt = Serial.read(); // Se leen los datos que se enviaron
+
+wifi.print(txt); // Imprime los datos introducidos
+
+}
+
+if (wifi.available()) //Igual que el serial, en este caso sería el módulo wifi que envíe datos cuando los reciba
+
+{txt = wifi.read(); // Se leen los datos en el módulo wifi
+
+Serial.print(txt); // Que se impriman los datos del módulo en el monitor serie
+
+}
+
+}
